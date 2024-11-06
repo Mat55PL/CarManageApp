c3b95e99e9a484a89a76a842f3c2a4ef624fa5b8
Mateusz "Mattu" Partyka <mateusz.pl671@gmail.com>
2024-11-06 23:06:38 +0100

    Create ICreateCar.tsx

---
+++ constants/Interfaces/ICreateCar.tsx
@@ -0,0 +1,9 @@
+export interface ICreateCar {
+    brand: string;
+    model: string;
+    vin: string;
+    year: number;
+    fuelType: number;
+    wheelType: number;
+    numberPlate: string;
+}
